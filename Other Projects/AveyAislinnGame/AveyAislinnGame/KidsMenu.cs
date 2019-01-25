45d41ed829a6def82eb1fc6200dd8476d87834e0
Christopher Gambaccini <CLGambaccini@student.fullsail.edu>
2019-01-25 14:59:59 -0500

    MenuClass Added
    I added a Menu Class to the program that will allow the user to choose between 8 games

---
+++ Other Projects/AveyAislinnGame/AveyAislinnGame/KidsMenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AveyAislinnGame
+{
+    class KidsMenu
+    {
+        public static string MenuSelection()
+        {
+
+            //Welcome user to program
+            Console.WriteLine("Welcome to the Kids Zone!!\r\nAre you ready for some fun?");
+
+            //display menu and prompt for selection
+            Console.WriteLine("\r\nBelow is a Menu of 8 Fun Games, choose wisely.\r\n\r\n1. Horses\r\n2. Dinosaurs\r\n3. LOL Dolls\r\n4. Peppa Pig,\r\n5. Unicorns\r\n6. Zombies\r\n7. Dog Ate My Homework\r\n8. But DAD");
+
+            //create variable to hold selection
+            string kidChoice = Console.ReadLine();
+
+            return Console.ReadLine();
+
+        }
+
+      
+
+
+
+
+
+    }
+}
