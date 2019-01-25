2abaa3d22f13ad92b8162c6d8d21d4d78b7c5e0e
Christopher Gambaccini <CLGambaccini@student.fullsail.edu>
2019-01-25 14:59:00 -0500

    AveryAislinnGame
    I'm starting a program that will encourage my two kids to play some games.

---
+++ Other Projects/AveyAislinnGame/AveyAislinnGame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AveyAislinnGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //function call from KidsMenu Class
+            KidsMenu.MenuSelection();
+
+
+
+
+        }
+    }
+}
