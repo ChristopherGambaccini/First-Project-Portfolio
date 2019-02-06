18a728e09712729302187f3b27ae05389d3e5c4a
Christopher Gambaccini <CLGambaccini@student.fullsail.edu>
2019-02-06 18:04:34 -0500

    New Class for New Project
    I created a new class for new project

---
+++ Other Projects/GambacciniChristopher_HardActivity/GambacciniChristopher_HardActivity/Scissors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace GambacciniChristopher_HardActivity
+{
+    public class Scissors
+    {
+        public Scissors()
+        {
+
+            //create mvariables
+            string mONeScissors;
+
+            decimal mTwoScissors;
+
+            double mThreeScissors;
+
+            //create 2 additional mvariables one with same datatype from each class
+            int mRockScissors;
+
+            float mPaperScissors;
+
+        }
+    }
+}
