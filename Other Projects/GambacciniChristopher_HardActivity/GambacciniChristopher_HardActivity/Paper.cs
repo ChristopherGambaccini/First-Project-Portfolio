c92c38e0296e0acb563ed2a274a52983ba7de8fe
Christopher Gambaccini <CLGambaccini@student.fullsail.edu>
2019-02-06 18:02:27 -0500

    New Paper Class for New Project
    I created a class for new project

---
+++ Other Projects/GambacciniChristopher_HardActivity/GambacciniChristopher_HardActivity/Paper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace GambacciniChristopher_HardActivity
+{
+    public class Paper
+    {
+        public Paper()
+        {
+
+
+            //create mvariables
+            double mOnePaper;
+
+            float mTwoPaper;
+
+            int mThreePaper;
+
+            //create 2 additional mvariables one with same datatype from each class
+            string mRockPaper;
+
+            double mScissorsPaper;
+
+
+        }
+    }
+}
