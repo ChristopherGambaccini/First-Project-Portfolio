d022604e2500f6b6ae80c9fa0bcd20ff400677e9
Christopher Gambaccini <CLGambaccini@student.fullsail.edu>
2019-02-06 18:03:50 -0500

    Created Rock Class for New Project
    I created a class for the New Project

---
+++ Other Projects/GambacciniChristopher_HardActivity/GambacciniChristopher_HardActivity/Rock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace GambacciniChristopher_HardActivity
+{
+    public class Rock
+    {
+        public Rock()
+        {
+
+            //create 3 mvariables
+             
+            string mOneRock;
+
+            int mTwoRock;
+
+            double mThreeRock;
+
+            //create 2 additional mvariables one with same datatype from each class
+            float mPaperRock;
+
+            decimal mScissorsRock;
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
