969ca4ca91d917b3bde1f157fcbcb7e848028b73
Christopher Gambaccini <CLGambaccini@student.fullsail.edu>
2019-02-08 08:53:30 -0500

    Updated Paper Class
    Added some more work to class: created constructors

--- Other Projects/GambacciniChristopher_HardActivity/GambacciniChristopher_HardActivity/Paper.cs
+++ Other Projects/GambacciniChristopher_HardActivity/GambacciniChristopher_HardActivity/Paper.cs
@@ -3,23 +3,82 @@ namespace GambacciniChristopher_HardActivity
 {
     public class Paper
     {
-        public Paper()
-        {
+       
 
 
             //create mvariables
-            double mOnePaper;
+            double monePaper;
 
-            float mTwoPaper;
+            float mtwoPaper;
 
-            int mThreePaper;
+            int mthreePaper;
 
             //create 2 additional mvariables one with same datatype from each class
-            string mRockPaper;
+            string mrockPaper;
 
-            double mScissorsPaper;
+            double mscissorsPaper;
+
+        //create constructor function 
+        //add parameters
+        public Paper(double _onepaper, float _twopaper, int _threepaper)
+        {
+
+            //set mvariables with parameter
+            monePaper = _onepaper;
+
+            mtwoPaper = _twopaper;
+
+            mthreePaper = _threepaper;
+
+            //output data to console
+            Console.WriteLine("Paper 3 parameter constructor");
 
 
         }
+
+        //create additional constructor function
+        public Paper(string _paperrock)
+        {
+
+            //set mvariables with parameter
+            mrockPaper= _paperrock;
+
+            //output to console
+            Console.WriteLine("Paper parameter: Rock");
+
+
+        }
+
+        //create additional constructor function
+        public Paper(double _scissorspaper)
+        {
+
+            //set mvariables with parameter
+            mscissorsPaper = _scissorspaper;
+
+
+            Console.WriteLine("Paper parameter: Scissors");
+
+
+        }
+        /*public static string Rock(float scissors)
+        {
+            float input = scissors;
+
+            string output = Convert.ToString(input);
+
+
+            string
+
+            Console.WriteLine("Paper method with parameter Scissors ");
+
+
+
+        }
+        */
+
+
     }
+
+   
 }
