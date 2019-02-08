32a26147f5ca4d0560123d7954a1477f51d16ac6
Christopher Gambaccini <CLGambaccini@student.fullsail.edu>
2019-02-08 08:55:26 -0500

    Updated Rock Class
    Added some more work to class: created constructors

--- Other Projects/GambacciniChristopher_HardActivity/GambacciniChristopher_HardActivity/Rock.cs
+++ Other Projects/GambacciniChristopher_HardActivity/GambacciniChristopher_HardActivity/Rock.cs
@@ -3,31 +3,68 @@ namespace GambacciniChristopher_HardActivity
 {
     public class Rock
     {
-        public Rock()
-        {
+
 
             //create 3 mvariables
-             
-            string mOneRock;
 
-            int mTwoRock;
+            string moneRock;
 
-            double mThreeRock;
+            int mtwoRock;
+
+            double mthreeRock;
 
             //create 2 additional mvariables one with same datatype from each class
-            float mPaperRock;
+            float mpaperRock;
 
-            decimal mScissorsRock;
-
-
+            decimal mscissorsRock;
 
 
+            //create constructor function
+            //add parameters 
+             public Rock(string _onerock, int _tworock, double _threerock)
+            {
+
+                //set mvariables with parameters
+
+                moneRock = _onerock;
+
+                mtwoRock = _tworock;
+
+                mthreeRock = _threerock;
+
+                //output data to console
+                Console.WriteLine("Rock 3 parameter constructor");
+
+
+            }
+            //create additional constructor function
+            public Rock(float _paper)
+            {
+
+                //set mvariables with parameter
+                mpaperRock = _paper;
+
+
+                Console.WriteLine("Rock parameter: Paper");
+
+
+            }
+            //create additional constructor function
+            public Rock(decimal _scissors)
+            {
+
+                //set mvariables with parameter
+                mscissorsRock = _scissors;
+
+
+                Console.WriteLine("Rock parameter: Scissors");
+
+
+            }
 
 
 
 
 
-
-        }
     }
 }
