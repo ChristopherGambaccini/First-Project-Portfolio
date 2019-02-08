adf9d2a522d09058290a779d630fcf4a65f39936
Christopher Gambaccini <CLGambaccini@student.fullsail.edu>
2019-02-08 08:54:55 -0500

    Updated Program Main
    I created 3 objects on Main, calling constructors

--- Other Projects/GambacciniChristopher_HardActivity/GambacciniChristopher_HardActivity/Program.cs
+++ Other Projects/GambacciniChristopher_HardActivity/GambacciniChristopher_HardActivity/Program.cs
@@ -6,9 +6,19 @@ namespace GambacciniChristopher_HardActivity
     {
         static void Main(string[] args)
         {
-           
+            //create first object by calling constructor
+            Rock firstObject = new Rock(" ", 0, 0);
 
-             
+            //create second object by calling constructor
+            Paper secondObject = new Paper(0, 0 ,0);
+
+            //create thrid object by calling constructor
+            Scissors thirdObject = new Scissors(" ", 0, 0 );
+
+
+
+
+
                
         }
     }
