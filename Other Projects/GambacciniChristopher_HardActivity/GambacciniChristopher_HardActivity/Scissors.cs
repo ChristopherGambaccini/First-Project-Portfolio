b2f353b4e74a4947f9fa891c22ba5c556ea24f1b
Christopher Gambaccini <CLGambaccini@student.fullsail.edu>
2019-02-08 08:55:55 -0500

    Updated Scissors Class
    Added some more work to class: created constructors

--- Other Projects/GambacciniChristopher_HardActivity/GambacciniChristopher_HardActivity/Scissors.cs
+++ Other Projects/GambacciniChristopher_HardActivity/GambacciniChristopher_HardActivity/Scissors.cs
@@ -3,21 +3,64 @@ namespace GambacciniChristopher_HardActivity
 {
     public class Scissors
     {
-        public Scissors()
-        {
+       
 
             //create mvariables
-            string mONeScissors;
+            string moneeScissors;
 
-            decimal mTwoScissors;
+            decimal mtwoScissors;
 
-            double mThreeScissors;
+            double mthreeScissors;
 
             //create 2 additional mvariables one with same datatype from each class
-            int mRockScissors;
+            int mrockScissors;
 
-            float mPaperScissors;
+            float mpaperScissors;
+
+
+        //create constructor function 
+        //add parameters
+        public Scissors(string _onescissors, decimal _twoscissors, double _threescissors)
+        {
+
+            //set mvariables with parameter
+            moneeScissors = _onescissors;
+
+            mtwoScissors = _twoscissors;
+
+            mthreeScissors = _threescissors;
+
+            //output data to console
+            Console.WriteLine("Scissors 3 parameter constructor");
+
 
         }
+        //create additional constructor function
+        public Scissors(int _rockscissors)
+        {
+
+            //set mvariables with parameter
+            mrockScissors = _rockscissors;
+
+            //output to console
+            Console.WriteLine("Scissors parameter: Rock");
+
+
+        }
+        //create additional constructor function
+        public Scissors(float _paperscissors)
+        {
+
+            //set mvariables with parameter
+            mpaperScissors = _paperscissors;
+
+            //output to console
+            Console.WriteLine("Scissors parameter: Paper");
+
+
+        }
+
+
+
     }
 }
