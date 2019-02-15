9fc7d4e17b642feaef667cbe3201730bbfd22c27
Christopher Gambaccini <CLGambaccini@student.fullsail.edu>
2019-02-15 13:23:46 -0500

    Updated Program
    I addded three method calls to main

--- Other Projects/GambacciniChristopher_HardActivity/GambacciniChristopher_HardActivity/Program.cs
+++ Other Projects/GambacciniChristopher_HardActivity/GambacciniChristopher_HardActivity/Program.cs
@@ -7,19 +7,19 @@ namespace GambacciniChristopher_HardActivity
         static void Main(string[] args)
         {
             //create first object by calling constructor
-            Rock firstObject = new Rock(" ", 0, 0);
+            Rock firstObject = new Rock(" " , 0 , 0);
 
             //create second object by calling constructor
-            Paper secondObject = new Paper(0, 0 ,0);
+            Paper secondObject = new Paper(0 , 0 , 0);
 
             //create thrid object by calling constructor
-            Scissors thirdObject = new Scissors(" ", 0, 0 );
+            Scissors thirdObject = new Scissors(" ", 0 , 0 );
 
+            Rock.CombineMethod(0 , 0);
 
+            Paper.CombineMethod("0" , 0);
 
-
-
-               
+            Scissors.CombineMethod(0, 0);
         }
     }
 }
