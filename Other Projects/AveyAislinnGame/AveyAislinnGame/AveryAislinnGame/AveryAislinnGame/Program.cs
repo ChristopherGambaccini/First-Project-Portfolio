281c8c3bf29abdf92fcb5fe9739fd7ef6435b2c9
Christopher Gambaccini <CLGambaccini@student.fullsail.edu>
2019-03-01 11:31:03 -0500

    Created new application
    This application main will call menu class

---
+++ Other Projects/AveyAislinnGame/AveyAislinnGame/AveryAislinnGame/AveryAislinnGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AveryAislinnGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome user to application
+            Console.WriteLine("Welcome to the Kidzone!!!\r\nAre you ready to have some fun?");
+
+            //Menu Class function call
+            Menu.MenuSelection();
+
+
+
+        }
+    }
+}
